499b17cb06530bbcbbbbcf02f615ee0ec3ff9952
vasanthrmkv <vasanthrmkv@gmail.com>
2025-11-05 15:31:16 +0000

    2nd commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Display current year automatically in footer
+// const year = new Date().getFullYear();
+// document.querySelector('footer p').innerHTML = `© ${year} Vasanth Raj Rajarajan | AWS & DevOps Engineer`;
+//
+// // Optional: Smooth scroll to sections
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//   anchor.addEventListener('click', function(e) {
+//       e.preventDefault();
+//           document.querySelector(this.getAttribute('href')).scrollIntoView({
+//                 behavior: 'smooth'
+//                     });
+//                       });
+//                       });
+//
